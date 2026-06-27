40d5c4491a60405b2d8eb7733e6280a410ad2e24
Nick Nguyen <nguyennick2005@gmail.com>
2026-06-27 13:43:20 -0700

    Add: cheapest-flight-path - submission-5

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        """
+
+        k+1 pass bellman ford
+        """
+        prices = [float('inf')]*n
+        prices[src]=0
+        for _ in range(k+1):
+            tmp_prices=prices.copy()
+
+            for s,d,p in flights:
+                if prices[s]==float('inf'):
+                    continue
+                if tmp_prices[s]+p < tmp_prices[d]:
+                    tmp_prices[d]=prices[s]+p
+            prices=tmp_prices
+        return prices[dst] if prices[dst]!=float('inf') else -1
+                
