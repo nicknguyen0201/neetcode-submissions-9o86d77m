c44744d19631295cf1ec6329d88a7d968323e9df
Nick Nguyen <nguyennick2005@gmail.com>
2026-08-21 21:10:28 -0700

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        def valid(r,c):
+            return r>=0 and r<n and c<n and c>=0
+        def safe(r,c):
+            for dr,dc in directions:
+                nr,nc=dr+r,dc+c
+                while valid(nr,nc):
+                    if grid[nr][nc]=='Q':
+                        return False
+                    nr+=dr
+                    nc+=dc
+            return True
+        def BT(r):
+            if r==n:
+                grid_copy=["".join(row) for row in grid]
+                res.append(grid_copy)
+                return 
+
+            for c in range(n):
+                if safe(r,c):
+                    grid[r][c]='Q'
+                    BT(r+1)
+                    grid[r][c]='.'
+              
+                    
+            
+        res=[]
+        directions=[(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]   
+        grid=[["."]*n for _ in range(n)]
+        BT(0)
+        return res
+            
+                
+        
