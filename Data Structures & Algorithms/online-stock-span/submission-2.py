e65781738b94b3ee7809de863af7722676487988
Nick Nguyen <nguyennick2005@gmail.com>
2026-09-06 22:32:40 -0700

    Add: online-stock-span - submission-2

---
+++ Data Structures & Algorithms/online-stock-span/submission-2.py
@@ -0,0 +1,33 @@
+from collections import deque
+class StockSpanner:
+
+    def __init__(self):
+        self.q=deque()
+        self.idx=0
+    def next(self, price: int) -> int:
+        
+        """
+        maintain a monotonic stack always decreasing
+        self idx=6
+        start:[(100,0),(85,)]
+        """
+        if not self.q:
+            self.q.append((price,0))
+            return 1
+        
+        self.idx+=1
+        while self.q:
+            if price>= self.q[-1][0]: #today price>= past price
+                self.q.pop()
+            else:
+                span=self.idx - self.q[-1][1]
+                self.q.append((price,self.idx))
+                return span
+        self.q.append((price, self.idx))#if no one left, then this is the biggest we habe seen ever
+        return self.idx+1
+
+            
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
